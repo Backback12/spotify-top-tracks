b27556d80634772aa4c197decc91887fbc8cc6d8
Backback12 <64019184+Backback12@users.noreply.github.com>
2022-10-27 16:54:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export var SPOTIFY_CLIENT_ID = "5e3191ca6ea84c92ada89e4ffe0a2c6c"
-export var SPOTIFY_REDIRECT_URI = "https://backback12.github.io/spotify-top-tracks/"
+//export var SPOTIFY_REDIRECT_URI = "https://backback12.github.io/spotify-top-tracks/"
+export var SPOTIFY_REDIRECT_URI = "https://backback12.github.io/"
